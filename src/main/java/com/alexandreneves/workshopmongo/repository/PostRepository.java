ad3738b22ab64c3a3c6b8ca16a645a15c80f4506
Alexandre Neves <bredaneves@gmail.com>
2023-05-16 15:35:58 -0300

    Consulta simples com query methods

--- src/main/java/com/alexandreneves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandreneves/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,17 @@
 package com.alexandreneves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.alexandreneves.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//o final do nome do método pode ser uma palavra chave que é interpretado
+	//vemos não haver implementação, pois o próprio nome indica a ação
+	//ex: Containing, GreaterThan, In
+	//o IgnoreCase é para não ser sensitivo
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/alexandreneves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/alexandreneves/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.alexandreneves.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexandreneves.workshopmongo.domain.Post;
+import com.alexandreneves.workshopmongo.resources.util.URL;
 import com.alexandreneves.workshopmongo.services.PostService;
 
 //a notação abaixo indica que é um componente do tipo Rest Controller
@@ -23,4 +27,13 @@ public class PostResource {
 		Post obj = service.findById(id);		
 		return ResponseEntity.ok().body(obj);
 	}	
+	
+	//o método abaixo irá receber o parâmetro text na chamada url de forma codificada
+	//será preciso decodificar antes de usar para fazer a busca
+	@RequestMapping(value="/titlesearch",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);			
+		return ResponseEntity.ok().body(list);
+	}
 }
