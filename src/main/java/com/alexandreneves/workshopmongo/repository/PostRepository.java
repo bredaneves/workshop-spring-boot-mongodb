61f343cdff6ff345654c812c9ca9aa07d861a303
Alexandre Neves <bredaneves@gmail.com>
2023-05-16 15:47:15 -0300

    Consulta simples com @Query

--- src/main/java/com/alexandreneves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandreneves/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,19 @@ package com.alexandreneves.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.alexandreneves.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//método personalizado
+	//nesse caso, precisamos da documentação do MongoDB para consultar os padrões regex
+	//https://docs.mongodb.com/manual/reference/operator/query/regex/
+	//?0 - indica o primeiro parâmetro passado no método
+	//options i - ignorar maiúsculas e minúsculas
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	//o final do nome do método pode ser uma palavra chave que é interpretado
 	//vemos não haver implementação, pois o próprio nome indica a ação
